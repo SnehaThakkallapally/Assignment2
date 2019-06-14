e764d59ce299ed33325da4f38396b503961f5a52
Sneha <tsbsdev@gmail.com>
2019-06-14 13:42:56 -0400

    I have added file third_program.py

---
+++ third_program.py
@@ -0,0 +1,10 @@
+special_char = ('hello&*$$world')
+count = special_char.count('$')
+print('$ :', count)
+
+count = special_char.count('*')
+print('* :', count)
+
+count = special_char.count('&')
+print('& :', count)
+
