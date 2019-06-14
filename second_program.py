807993af895b474e9b9124edbb74bcd801c75445
Sneha <tsbsdev@gmail.com>
2019-06-14 13:42:20 -0400

    I have added file second_program.py

---
+++ second_program.py
@@ -0,0 +1,7 @@
+#list = [1,2,3,4,5,6]
+#items = [1, 2, 3, 5, 6]
+items = range(1, 22) 
+print(list(items))
+even_pair = [(items[i],items[j]) for i in range(len(items)) for j in range(i+1, len(items)) if((items[i]+items[j])%2 == 0) ]
+
+print(even_pair)
