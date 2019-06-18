abd4c7076ac02be3a3e5d983ed1ab6c1c7e95430
SnehaThakkallapally <51350556+SnehaThakkallapally@users.noreply.github.com>
2019-06-18 10:57:56 -0400

    Update second_program.py

--- second_program.py
+++ second_program.py
@@ -1,7 +1,8 @@
 #list = [1,2,3,4,5,6]
 #items = [1, 2, 3, 5, 6]
 items = range(1, 22) 
-print(list(items))
-even_pair = [(items[i],items[j]) for i in range(len(items)) for j in range(i+1, len(items)) if((items[i]+items[j])%2 == 0and (items[i]+items[j]) in range(22))  ]
+#print(list(items))
+even_pair = [(items[i],items[j]) for i in range(len(items)) for j in range(i+1, len(items)) 
+if((items[i]+items[j])%2 == 0 and (items[i]+items[j]) in range(22)) ]
 
 print(even_pair)
